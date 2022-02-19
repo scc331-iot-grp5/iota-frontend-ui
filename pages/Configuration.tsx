89336e310ce0ee6247af00152ffa27a6dc1e08ea
Nima1010 <78490757+Nima1010@users.noreply.github.com>
2022-02-19 19:07:39 +0000

    styled it abit more. in the comments when we actually put it all together we can uncomment them to make the style nicer

--- pages/Configuration.tsx
+++ pages/Configuration.tsx
@@ -8,7 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Pop from '../components/popupbutton';
 import Button from '@mui/material/Button';
-import { Device } from 'types/device';
+import { Device } from 'types/device'; 
 import { Type } from 'types/device';
 // import AppBar from '@/components/app-bar';
 // import { useRouter } from 'next/router';
@@ -17,7 +17,8 @@ interface State {
   devices: Device[];
   device_Type: Type[];
   // device_types: Device.Type[];
-}
+  //deviceData: Device[];
+} 
 
 // function dashToDeviceGrid(dash: Device): JSX.Element {
 //   const rows =[
@@ -30,18 +31,49 @@ interface State {
 //     )
 //     }
 
-function createData(id: number, CustomName: string, Type: string) {
+function createData(
+    id: number,
+    CustomName: string,
+    Type: string,
+    
+    
+  )
+ {
   return { id, CustomName, Type };
 }
 
 const rows = [
-  createData(1000, 'busMicrobit', 'bus'),
-  createData(1111, '-', 'human'),
-  createData(2222, 'humanMicrobit', 'car'),
-  createData(3333, 'CustomName', 'bus'),
-  createData(4444, '-', 'car'),
+  createData(1000, "busMicrobit", "bus"),
+  createData(1111, "-", "human"),
+  createData(2222, "humanMicrobit", "car"),
+  createData(3333, "CustomName", "bus"),
+  createData(4444, "-", "car"),
 ];
 
+
+function fetchValues(
+  values: State,
+  setValues: React.Dispatch<React.SetStateAction<State>>
+): void {
+  fetch('http://localhost:1880/Device')
+    .then((res) => res.json())
+    .then(
+      (res) => {
+        console.log('Configuration data fetched');
+        if (res === {}) {
+          console.log('no data retrieved');
+          return;
+        }
+        setValues({ ...values, devices: res });
+      },
+      (err) => console.log('Configuration data fetch failed', err)
+    );
+}
+
+
+
+
+ 
 export default function BasicTable() {
   // const router = useRouter();
   // const [values, setValues] = React.useState<State>({
@@ -51,18 +83,34 @@ export default function BasicTable() {
   //   },
   // });
   return (
+    
     // <AppBar />
+    // <Head>
+    //     <title>IOTA: Configuration</title>
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
 
+    // <IconButton
+    //     aria-label="refresh"
+    //     size="small"
+    //     onClick={() => fetchValues(values, setValues)}
+    //   >
+    //     <Refresh fontSize="small" />
+    //   </IconButton>
+
+    
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        
         <TableHead>
-          <TableRow>
+          <TableRow> 
             <TableCell>ID</TableCell>
             <TableCell align="right">Name</TableCell>
             <TableCell align="right">Type</TableCell>
-            <TableCell>Configuration</TableCell>
+            <TableCell >Configuration</TableCell>
           </TableRow>
         </TableHead>
+
         <TableBody>
           {rows.map((row) => (
             <TableRow
@@ -74,8 +122,8 @@ export default function BasicTable() {
               </TableCell>
               <TableCell align="right">{row.CustomName}</TableCell>
               <TableCell align="right">{row.Type}</TableCell>
-
-              <Pop />
+              
+              <Pop/>
             </TableRow>
           ))}
         </TableBody>
