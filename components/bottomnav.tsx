84bd5913b4c44667824a3ed40d8b716528f5f1cc
Nima1010 <78490757+Nima1010@users.noreply.github.com>
2022-02-25 10:52:38 +0000

    just something extra to spice up the pages

---
+++ components/bottomnav.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FolderIcon from '@mui/icons-material/Folder';
+import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import HomeIcon from '@mui/icons-material/Home';
+export default function LabelBottomNavigation() {
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation sx={{ width: 500, position:'absolute', bottom:1 }} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Home"
+        value="recents"
+        icon={<HomeIcon />}
+      />
+      <BottomNavigationAction
+        label="Users"
+        value="favorites"
+        icon={<AccountBoxIcon />}
+      />
+      <BottomNavigationAction
+        label="Devices"
+        value="nearby"
+        icon={<DevicesOtherIcon />}
+      />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    </BottomNavigation>
+  );
+}
