b9df2e0a7e33b044aecd2cf2effd2387450c9d13
Nima1010 <78490757+Nima1010@users.noreply.github.com>
2022-03-04 21:24:15 +0000

    scatter chart component. will need to import dev express to use this

---
+++ components/scatter-chart.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import {
+  ArgumentAxis,
+  ValueAxis,
+  BarSeries,
+  Chart,
+  LineSeries,
+  ScatterSeries,
+} from '@devexpress/dx-react-chart-material-ui';
+import { ValueScale } from '@devexpress/dx-react-chart';
+
+interface IDataItem {
+  month: string,
+  device: number,
+  temp: number,
+}
+
+const chartData: IDataItem[] = [
+  { month: 'Jan', device: 50, temp: 987 },
+  { month: 'Feb', device: 100, temp: 3000 },
+  { month: 'March', device: 30, temp: 1100 },
+  { month: 'April', device: 107, temp: 7100 },
+  { month: 'May', device: 95, temp: 4300 },
+  { month: 'June', device: 150, temp: 7500 },
+];
+
+export default class Demo extends React.Component<object, object> {
+  public render(): React.ReactNode {
+    return (
+      <Paper>
+        <Chart
+          data={chartData}
+        >
+          <ValueScale name="device" />
+          <ValueScale name="temp" />
+
+          <ArgumentAxis />
+          <ValueAxis scaleName="device" showGrid={false} showLine={true} showTicks={true} />
+          <ValueAxis scaleName="temp" position="right" showGrid={false} showLine={true} showTicks={true} />
+
+          <ScatterSeries
+            valueField="temp"
+            argumentField="month"
+          />
+        </Chart>
+      </Paper>
+    );
+  }
+}
