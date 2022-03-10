769eb2898c31384883135045e58a8dd431a03c6f
Lewis Miller <l.w.miller@lancaster.ac.uk>
2022-03-10 15:29:28 +0000

    remove testing on commit - too many emails

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { dataAPI } from '../redux/data-api';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import styles from 'index.module.css';
+import styles from './index.module.css';
 import {
   IconButton,
   OutlinedInput,
